a6a244a1f97be039d8770fc56d95149635099aa3
Eber Johns C D <eberjohnspro@gmail.com>
2025-07-31 23:28:16 +0530

    feat: graph with points and line completed

---
+++ canvas/perceptron.js
@@ -0,0 +1,121 @@
+const canvas = document.getElementById("canvas");
+
+const line_element = document.getElementById("line");
+
+let width = 400;//window.innerWidth;
+let height = 400;//window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+canvas.style.background = "black";
+
+/** @type {CanvasRenderingContext2D} */
+const ctx = canvas.getContext("2d");
+
+function transform_x(x){
+    return x += width/2;
+}
+function transform_y(y){
+    return y += height/2;
+}
+
+let m = 0;
+let c = 0;
+
+line_element.textContent = `m = ${m} | c = ${c}`;
+
+function line(x){
+    return m*x+c;
+}
+
+addEventListener('keydown',function(event){
+    switch(event.key){
+        case 'ArrowUp': m++; break;
+        case 'ArrowDown': m--; break;
+        case 'ArrowLeft': c+=10; break;
+        case 'ArrowRight': c-=10; break;
+        default: return;
+    }
+
+    line_element.textContent = `m = ${m} | c = ${c}`;
+    
+    ctx.clearRect(0, 0, width, height);
+    draw_line();
+    draw_points();
+});
+
+class Point{
+    constructor(x,y,color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    draw(){
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x,this.y,5,0,Math.PI*2);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+let point = [];
+let noOfPoints = 100;
+
+function draw_line(){
+    let x1 = -width / 2;
+    let x2 = width / 2;
+
+    let y1 = m * x1 + c;
+    let y2 = m * x2 + c;
+
+    ctx.strokeStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(transform_x(x1), transform_y(y1));
+    ctx.lineTo(transform_x(x2), transform_y(y2));
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function setup(){
+    for(let i = 0; i < noOfPoints; i++){
+        //get value between -1 & 1 and multiply with width or height by 2
+        let x = (Math.random()*2 - 1) * (width/2);
+        let y = (Math.random()*2 - 1) * (height/2);
+
+        let color = (y-line(x)>=0) ? "green" : "red";
+
+        // for passing as x & y for drawing trasform to the cartesian plane
+        point[i] = new Point(transform_x(x),transform_y(y),color);
+        point[i].draw();
+    }
+}
+
+function draw_points(){
+    for(let i = 0; i < noOfPoints; i++){
+        //trasform back to cartesian plane 
+        let x = point[i].x - width/2;
+        let y = point[i].y - height/2;
+
+        //check where the point is relative to line
+        point[i].color = (y -line(x)>=0) ? "green" : "red";
+        point[i].draw();
+    }
+}
+
+draw_line();
+setup();
+
+// function animate(){
+//     context.clearRect(0,0,canvas.width,canvas.height);
+
+//     draw_line();
+//     draw_points();
+
+//     requestAnimationFrame(animate);
+// }
+
+// animate();
